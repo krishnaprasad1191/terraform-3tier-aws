a44b1641fcec76f1c918cac7a63d48567cec0d7d
KP <krishnaprasad8099@gmail.com>
2025-10-17 13:57:05 +0530

    initial VPC

---
+++ var.tf
@@ -0,0 +1,14 @@
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "cidr_block" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "vpc_name" {
+  type    = string
+  default = "UKP"
+}
