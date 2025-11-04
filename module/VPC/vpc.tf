ab5f1b173bfa2574840e2fda9f8ab163ae66cac4
KP <krishnaprasad8099@gmail.com>
2025-11-04 15:20:02 +0530

    Added pub RT,assoc with pub Sub

--- module/VPC/vpc.tf
+++ module/VPC/vpc.tf
@@ -30,3 +30,21 @@ resource "aws_subnet" "Private_Subnets" {
   availability_zone = each.value.az
   tags = { Name = "${var.Vpc_Name}_${each.key}" }
 }
+
+# Route_Tables
+
+resource "aws_route_table" "Pub_RT" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block=var.Internet
+    gateway_id= aws_internet_gateway.igw.id
+  }
+  tags = { Name= "${var.Vpc_Name}_Pub_RT"}
+}
+
+resource "aws_route_table_association" "RT_Pubsub_assoc" {
+
+  for_each = aws_subnet.Public_Subnets  #taking public subnets
+  route_table_id = aws_route_table.Pub_RT.id
+  subnet_id = each.value.id
+}
